eb6a9b4f6d31c6ca541c83d0f7a01d78627be890
9yubean2 <dlrbqls3024@swu.ac.kr>
2023-01-13 12:31:49 +0900

    01 기본 타입 - 튜플, 객체, 진위값

--- 01.Fundementals/01.type-basic.ts
+++ 01.Fundementals/01.type-basic.ts
@@ -4,7 +4,29 @@
 //TS 문자열 선언
 let str: string = 'hello';
 
+//TS 숫자
 let number: number = 10;
-let arr:Array<number> = [1,2,3];
+
+//TS 배열
+let arr:Array<number> = [1, 2, 3];
 //let heroes:Array<string> = ['Capt', 'Thor', 'Hulk', 10]; //Type 'number' is not assignable to type 'string'.
 let heroes:Array<string> = ['Capt', 'Thor', 'Hulk'];
+let items: number[] = [1, 2, 3];
+
+//TS 튜플
+let address: [string, number] = ['gangnam', 100];
+
+//TS 객체
+let obj: object = {};
+let person: object = {
+    name: 'capt',
+    age: 100
+};
+
+let person2: {name: string, age: number} = {
+    name: 'thor',
+    age: 1000
+};
+
+//TS 진위값
+let show: boolean = true;
