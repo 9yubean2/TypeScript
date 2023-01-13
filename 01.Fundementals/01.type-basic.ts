577ffe88c3c3a6d4f8d43929eb7e247b47a17407
9yubean2 <dlrbqls3024@swu.ac.kr>
2023-01-13 12:27:14 +0900

    01-1 기본타입

---
+++ 01.Fundementals/01.type-basic.ts
@@ -0,0 +1,10 @@
+//JS 문자열 선언
+//let str='hello';
+
+//TS 문자열 선언
+let str: string = 'hello';
+
+let number: number = 10;
+let arr:Array<number> = [1,2,3];
+//let heroes:Array<string> = ['Capt', 'Thor', 'Hulk', 10]; //Type 'number' is not assignable to type 'string'.
+let heroes:Array<string> = ['Capt', 'Thor', 'Hulk'];
